44f13c31774fa90cd7da4ff961810feebda78e24
sakasanokuma <moonsunphoenix@gmail.com>
2022-06-05 22:53:42 +0900

    add args

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import read_scb
+import sys
 
-print(read_scb.get_lines_by_page())
+if len(sys.argv) < 3:
+    print("引数の数が不正です")
+    sys.exit()
+else:
+    page_title_base = sys.argv[1]
+    max_chapter = int(sys.argv[2])
+
+for no in range(1, max_chapter + 1):
+    page_title = page_title_base + str(no)
+    print(read_scb.get_lines_by_page(page_title))
--- read_scb.py
+++ read_scb.py
@@ -7,12 +7,10 @@ const.read('conf/const.ini')
 scb_url = const.get('common','scb_url')
 cookie = const.get('common','cookie')
 break_text = '------------'
-
-scb_url = scb_url + '休んだ日には雨が降る1'
 cookies={'connect.sid': cookie}
 
-def get_lines_by_page():
-    r = requests.get(scb_url, cookies=cookies)
+def get_lines_by_page(page_title):
+    r = requests.get(scb_url + page_title, cookies=cookies)
     # 一行目はタイトルなので、タイトル以降
     lines = r.json()['lines'][1:]
     result = ''
@@ -23,5 +21,3 @@ def get_lines_by_page():
         # print(line['text'].replace('[', '').replace(']', ''))
         result += line['text'].replace('[', '').replace(']', '') + '\n'
     return result
-    
-    # print(r.json()['lines']);
